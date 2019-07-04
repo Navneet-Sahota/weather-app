8d0a90778fd0fbcd3659e08b3837018d0c90b466
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-04 08:31:19 +0530

    use argv to get location

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("New Delhi", (err, data) => {
-	if (err) {
-		return console.log(err);
-	}
-	forecast(data.latitude, data.longitude, (err, forecastData) => {
+const location = process.argv[2];
+
+const weatherReport = location => {
+	geocode(location, (err, data) => {
 		if (err) {
 			return console.log(err);
 		}
-		console.log(data.location);
-		console.log(forecastData);
+		forecast(data.latitude, data.longitude, (err, forecastData) => {
+			if (err) {
+				return console.log(err);
+			}
+			console.log(data.location);
+			console.log(forecastData);
+		});
 	});
-});
+};
+
+if (location) {
+	weatherReport(process.argv[2]);
+} else {
+	console.log("Please provide a location to get weather report.");
+}
