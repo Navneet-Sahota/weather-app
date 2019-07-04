e5e59705920a197d5875863b28cf4df2af4bf77c
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-04 08:01:32 +0530

    add forecast method

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
+
+forecast(37.8267, -122.4233, (err, response) => {
+	console.log("Error: ", err);
+	console.log("Response: ", response);
+});
 
 geocode("New Delhi", (err, response) => {
 	console.log("Error: ", err);
---
+++ utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const forecast = (lat, long, callback) => {
+	const url = `https://api.darksky.net/forecast/c2d58445b9a9fe39730b4c734962e6db/${encodeURIComponent(
+		lat
+	)},${encodeURIComponent(long)}?units=si`;
+
+	request({ url, json: true }, (err, response, body) => {
+		if (err) {
+			callback("Unable to connect to weather service", null);
+		} else if (body.error) {
+			callback("Unable to detect location", null);
+		} else {
+			const { temperature, precipProbability } = body.currently;
+			const { summary } = body.daily;
+			callback(
+				null,
+				`${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`
+			);
+		}
+	});
+};
+
+module.exports = forecast;
