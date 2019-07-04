41bc7283cd2170f9a9c4da061e6bd2f03705df5f
Navneet Sahota <navneet.sahota@outlook.com>
2019-07-04 08:27:27 +0530

    make async call to get weather report

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-forecast(37.8267, -122.4233, (err, response) => {
-	console.log("Error: ", err);
-	console.log("Response: ", response);
-});
-
-geocode("New Delhi", (err, response) => {
-	console.log("Error: ", err);
-	console.log("Response: ", response);
+geocode("New Delhi", (err, data) => {
+	if (err) {
+		return console.log(err);
+	}
+	forecast(data.latitude, data.longitude, (err, forecastData) => {
+		if (err) {
+			return console.log(err);
+		}
+		console.log(data.location);
+		console.log(forecastData);
+	});
 });
